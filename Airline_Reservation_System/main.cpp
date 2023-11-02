effc4656d5d129342f96c7454949181c8266c02b
Show Wai Yan <showwaiyan555@gmail.com>
2023-11-02 12:09:44 +0800

    section chosing and yes Or no chosing checking function finished

---
+++ Airline_Reservation_System/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int acceptInput(string flag); // accepting user input
+bool checkInput(string inputData, string flag); // viladation checking for user input
+
+void clearScreen(); // Clear terminal or comand prompt 
+
+int main() {
+    clearScreen();
+    cout << acceptInput("section-chosing") << endl;
+
+    return 0;
+}
+
+int acceptInput(string flag) {
+    string userInput;
+    bool validInput = false;
+
+    do {
+        if (flag == "section-chosing") {
+            cout << "Enter your dedicated airplane seciont seat.\n";
+            cout << "Type '1' for Smoking section :\n";
+            cout << "Type '2' for Non-smoking section :\n";
+            cout << "Enter here: ";
+            cin >> userInput;
+
+            clearScreen();
+
+            validInput = checkInput(userInput, flag);
+
+        }
+        else if (flag == "yesORno-chosing") {
+
+        }
+        else {
+            cout << "Inavlid Flag!" << endl;
+            return 0;
+        }
+
+    } while (!validInput);
+
+    return stoi(userInput);
+}
+
+bool checkInput(string inputData, string flag) {
+
+    if (flag == "section-chosing" || flag == "yesORno-chosing") {
+        if (inputData.size() > 1) {
+            // Valid input should be only one character
+            // which is '1' and '2'
+            // Otherwise, invalid input
+            cout << "Please enter appropriate input that is descripted by program!\n" << endl;
+            return false;
+        }
+        else if (inputData == "1" || inputData == "2") return true;
+        else {
+            cout << "Please enter only valid value options provided by the program\n" << endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+void clearScreen()
+{
+#if defined _WIN32
+    system("cls");
+    //clrscr(); // including header file : conio.h
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+#elif defined (__APPLE__)
+    system("clear");
+#endif
+}
